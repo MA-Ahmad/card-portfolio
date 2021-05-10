1febe038130f03ecb2302489d87c469c3b931428
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-10 12:27:29 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,27 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import * as React from "react";
+import { ChakraProvider, Box, Text, Code, theme } from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
+    <Box p={3}>
+      <ColorModeSwitcher justifySelf="flex-end" />
+      <Box
+        fontSize="xl"
+        position="fixed"
+        left={0}
+        right={0}
+        bottom={0}
+        top={0}
+        textAlign="center"
+        height="max-content"
+        width="max-content"
+        margin="auto"
+      >
+        <Text>
+          Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+        </Text>
+      </Box>
     </Box>
   </ChakraProvider>
-)
+);
