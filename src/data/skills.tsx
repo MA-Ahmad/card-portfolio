20a908ed8ec8bccac289047e3ea841651600140b
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-11 08:30:01 -0700

    Create skills.tsx

---
+++ src/data/skills.tsx
@@ -0,0 +1,64 @@
+import ruby from "assets/images/skills/ruby.png";
+import react from "assets/images/skills/react.png";
+import rails from "assets/images/skills/rails.png";
+import javascript from "assets/images/skills/javascript.png";
+import typescript from "assets/images/skills/typescript.png";
+import python from "assets/images/skills/python.png";
+import postgres from "assets/images/skills/postgresql.png";
+import mysql from "assets/images/skills/mysql.png";
+import aws from "assets/images/skills/aws.png";
+
+export const skills = [
+  {
+    name: "ROR",
+    link: "https://rubyonrails.org/",
+    image: rails
+  },
+  {
+    name: "React",
+    link: "https://www.typescriptlang.org/",
+    image: react
+  },
+  {
+    name: "Ruby",
+    link: "https://www.ruby-lang.org/en/",
+    image: ruby
+  },
+  {
+    name: "Python",
+    link: "https://www.python.org/",
+    image: python
+  },
+  {
+    name: "JS",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    image: javascript
+  },
+  {
+    name: "TS",
+    link: "https://www.typescriptlang.org/",
+    image: typescript
+  },
+  {
+    name: "Postgres",
+    description: "Database",
+    link: "https://www.postgresql.org/",
+    type: "database",
+    image: postgres
+  },
+  {
+    name: "Mysql",
+    description: "Database",
+    link: "https://www.mysql.com/",
+    type: "database",
+    image: mysql
+  },
+  {
+    name: "AWS",
+    description: "Devops",
+    link: "https://aws.amazon.com/",
+    type: "devops",
+    image: aws
+  },
+  
+];
