73ac835d9e26fc2b2b52414412245cc9910d84d5
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-11 08:53:07 -0700

    Update card.tsx

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,70 +6,76 @@ import { BiArrowBack } from "react-icons/bi";
 import Skills from "./skills";
 import MainScreen from "./main-screen";
 import { container } from "./animation-variants";
+import Footer from "./footer";
 
 const Card = () => {
   const bg = useColorModeValue("white", "#2f3244");
   const [type, setType] = React.useState("profile");
 
   return (
-    <Box
-      fontSize="xl"
-      position="fixed"
-      left={0}
-      right={0}
-      bottom={0}
-      top={0}
-      textAlign="center"
-      height="max-content"
-      width="max-content"
-      margin="auto"
-    >
+    <>
       <Box
-        maxH={"400px"}
-        minH={"354px"}
-        w="345px"
-        boxShadow={"lg"}
-        rounded={"md"}
-        p={6}
-        overflow={"hidden"}
-        cursor="pointer"
-        _hover={{ boxShadow: "lg" }}
-        bg={bg}
-        role="group"
+        fontSize="xl"
+        position="fixed"
+        left={0}
+        right={0}
+        bottom={0}
+        top={0}
+        textAlign="center"
+        height="max-content"
+        width="max-content"
+        margin="auto"
       >
-        {type === "profile" ? (
-          <motion.div variants={container} initial="hidden" animate="visible">
-            <MainScreen />
-          </motion.div>
-        ) : (
-          <Skills />
-        )}
-        <HStack
-          position={"absolute"}
-          left={0}
-          bottom={0}
-          p={1}
-          py={1}
-          width={"100%"}
-          justify={"flex-end"}
+        <Box
+          maxH={"400px"}
+          minH={"354px"}
+          w="345px"
+          boxShadow={"lg"}
+          rounded={"md"}
+          p={6}
+          overflow={"hidden"}
+          cursor="pointer"
+          _hover={{ boxShadow: "lg" }}
+          bg={bg}
+          role="group"
         >
-          <motion.div
-            whileHover={{
-              scale: [1, 2, 2, 1, 1],
-              rotate: [0, 0, 270, 270, 0],
-              borderRadius: ["20%", "20%", "50%", "50%", "20%"]
-            }}
+          {type === "profile" ? (
+            <motion.div variants={container} initial="hidden" animate="visible">
+              <MainScreen />
+            </motion.div>
+          ) : (
+            <Skills />
+          )}
+          <HStack
+            position={"absolute"}
+            left={0}
+            bottom={0}
+            p={1}
+            py={1}
+            width={"100%"}
+            justify={"flex-end"}
           >
-            <Icon
-              as={type === "profile" ? RiArrowRightLine : BiArrowBack}
-              w={6}
-              h={6}
-              onClick={() => setType(type === "profile" ? "skills" : "profile")}
-            />
-          </motion.div>
-        </HStack>
+            <motion.div
+              whileHover={{
+                scale: [1, 2, 2, 1, 1],
+                rotate: [0, 0, 270, 270, 0],
+                borderRadius: ["20%", "20%", "50%", "50%", "20%"]
+              }}
+            >
+              <Icon
+                as={type === "profile" ? RiArrowRightLine : BiArrowBack}
+                w={6}
+                h={6}
+                onClick={() =>
+                  setType(type === "profile" ? "skills" : "profile")
+                }
+              />
+            </motion.div>
+          </HStack>
+        </Box>
       </Box>
-    </Box>
+      <Footer />
+    </>
   );
 };
 
