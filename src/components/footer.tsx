9ef1d01b065456594bff716d6d5be0cf933d92ad
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-11 08:53:10 -0700

    Create footer.tsx

---
+++ src/components/footer.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box
+    //   fontSize="xl"
+      position="fixed"
+      left={0}
+      right={0}
+      bottom={0}
+      textAlign="center"
+      height="max-content"
+      width="max-content"
+      margin="auto"
+      fontSize="md"
+      p={3}
+    >
+      Website built with
+      <Box
+        as="span"
+        mx="2"
+        _before={{
+          cursor: "default",
+          content: '"❤️"'
+        }}
+        _hover={{
+          _before: {
+            content: '"☕️"'
+          }
+        }}
+      />
+      in Pakistan{"  "}🇵🇰
+    </Box>
+  );
+};
+
+export default Footer;
