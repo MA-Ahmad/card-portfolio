cc231d07594f5f85cbe66b3b82e158216cf0f77f
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-11 08:29:40 -0700

    Create animation-variants.tsx

---
+++ src/components/animation-variants.tsx
@@ -0,0 +1,20 @@
+
+export const container = {
+  hidden: { opacity: 1, scale: 0 },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delayChildren: 0.3,
+      staggerChildren: 0.2
+    }
+  }
+};
+
+export const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
