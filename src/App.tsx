0058024478281734c3e383549ee2e06b98ab01d2
MA-Ahmad <muhammad.ahmad8043@gmail.com>
2021-05-28 05:25:17 -0700

    extended default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import * as React from "react";
-import { ChakraProvider, Box, theme } from "@chakra-ui/react";
+import { ChakraProvider, Box, extendTheme } from "@chakra-ui/react";
 import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import { mode } from "@chakra-ui/theme-tools"
 import Card from "components/card";
+
+const theme = extendTheme({
+  styles: {
+    global: (props) => ({
+      body: {
+        color: mode("gray.700", "whiteAlpha.900")(props),
+        bg: mode("#f5f0ea", "gray.900")(props),
+      }
+    })
+  }
+});
 
 export const App = () => {
   return (
